e63a4449cf6cec3a85db74a63fb4d26c58bd5193
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-05-17 19:39:30 -0700

    changes

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+# AWS hashicat URL
+output "aws" {
+    value = module.aws.catapp_url
+}
+
+# Azure hashicat URL
+output "azure_hashicat" {
+    value = module.azure.catapp_url
+}
+
+# GCP hashicat URL
+output "gcp_hashicat" {
+    value = module.gcp.catapp_url
+}
